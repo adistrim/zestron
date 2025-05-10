1402a496be227ffc0fb46d64ded27c88e7ccd916
Aditya Raj <adistrim.dev@gmail.com>
2025-05-10 21:38:15 +0530

    serving static html on root route

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"zestron-server/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,21 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+
+	router.Static("/public", "./public")
+	router.StaticFile("/favicon.ico", "./public/favicon.ico")
+
+	router.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/404")
+	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.File("./public/index.html")
+	})
+
+	router.GET("/404", func(c *gin.Context) {
+		c.File("./public/404.html")
+	})
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
